5831db4d71b39b7de11f67feb2a1c66c2ec3d48f
Richard Scheglmann <r.scheglmann@gmail.com>
2023-05-06 22:56:43 +0200

    Add hamburger nav

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 
 import "./simple.css";
 
+const ce_hamburger = document.querySelector(".ce_hamburger");
+ce_hamburger.onclick = function () {
+    const ce_nav = document.querySelector(".ce_nav");
+    if (ce_nav.className === "ce_nav") {
+        ce_nav.className += " ce_open";
+        ce_hamburger.innerHTML = "X";
+    } else {
+        ce_nav.className = "ce_nav";
+        ce_hamburger.innerHTML = "...";
+    }
+}
+console.log("in")
